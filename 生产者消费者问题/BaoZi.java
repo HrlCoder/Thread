46a386a5a25e969dfa29b0dbb24d71679e8ee990
HrlCoder <634464975@qq.com>
2021-01-01 21:11:21 +0800

    生产者消费者问题

---
+++ 生产者消费者问题/BaoZi.java
@@ -0,0 +1,9 @@
+package HMThread.生产者消费者问题;
+
+public class BaoZi {
+    String pi;
+    String xian;
+    boolean flag = false;
+
+
+}
---
+++ 生产者消费者问题/Demo.java
@@ -0,0 +1,9 @@
+package HMThread.生产者消费者问题;
+
+public class Demo {
+    public static void main(String[] args) {
+        BaoZi bz = new BaoZi();
+        new BaoZiPu(bz).start();
+        new ChiHuo(bz).start();
+    }
+}
